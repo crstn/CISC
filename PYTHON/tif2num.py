f9de1ec4fb8885ec551c776b8cff61f6ad3d4682
Carsten Keßler <carsten.kessler@hunter.cuny.edu>
2015-11-19 19:12:34 -0500

    Added sanity check for raster dimensions

--- PYTHON/tif2num.py
+++ PYTHON/tif2num.py
@@ -11,37 +11,8 @@ for f in files:
     src = gdal.Open(f, gdal.GA_Update)
     band = src.GetRasterBand(1)
     imarray = np.array(band.ReadAsArray())
+    # Make sure the numpy array all have the same dimensions:
+    print f
+    print imarray.shape
+
     np.save(f, imarray)
-
-
-# src = gdal.Open('/Users/carsten/Dropbox/Code/CISC/Data/NumpyLayers/NationOutlines.tif', gdal.GA_Update)
-# band = src.GetRasterBand(1)
-# imarray = np.array(band.ReadAsArray())
-# # convert to 16-bit int, large enough for our data -> save space on disk
-# np.save('NationOutlines', imarray.astype('int16'))
-
-
-# # urban/rural
-# im = Image.open('UrbanRural.tif')
-# imarray = np.array(im)
-# # convert to 8-bit int, large enough for our data -> save space on disk
-# np.save('UrbanRural', imarray.astype('int8'))
-
-# national boundaries
-# this one did not load using PIL (no idea why), so we're doing gdal here
-# src = gdal.Open('/Users/carsten/Dropbox/Code/CISC/Data/NumpyLayers/NationOutlines.tif', gdal.GA_Update)
-# band = src.GetRasterBand(1)
-# imarray = np.array(band.ReadAsArray())
-# # convert to 16-bit int, large enough for our data -> save space on disk
-# np.save('NationOutlines', imarray.astype('int16'))
-
-# # population numbers
-# im = Image.open('Population2000.tif')
-# imarray = np.array(im)
-# # we'll change this from float to int, no point in keeping "half people"
-# np.save('Population2000', imarray.astype('int'))
-#
-# im = Image.open('Population2010_clipped.tif')
-# imarray = np.array(im)
-# # we'll change this from float to int, no point in keeping "half people"
-# np.save('Population2010', imarray.astype('int'))
