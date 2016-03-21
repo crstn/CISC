399393102c4bf26664da4ad74081e8064f0c413a
Carsten Keßler <carsten.kessler@hunter.cuny.edu>
2016-03-20 22:10:43 -0400

    Made the whole thing a bit more modular

--- PYTHON/PopFunctions.py
+++ PYTHON/PopFunctions.py
@@ -3,144 +3,33 @@ import os, datetime, sys, operator, logging, math, csv
 import numpy as np
 from datetime import datetime
 
-# This will get rid of some floating point issues (well, reporting of them!)
-old_settings = np.seterr(invalid="ignore")
-
-# run the simulation only on specific countries, or on all countries found in the input files?
-runCountries = "all"
-#runCountries = ["392", "764"] # look up the country codes in the WUP or WTP csv files; make sure to put in quotes!
-
-# this will be added to the output file names, useful for testing.
-postfix = "test2"
-
-# perform a linear projection first, then adjust randomly?
-projectingLinear = False
-
-# how many times do we want to simulate?
-RUNS = 1
-# this is a GeoTIFF that we'll use as a reference for our output - same bbox, resolution, CRS, etc.
-referencetiff = ""
-
 # some constants:
 ruralCell = 1
 urbanCell = 2
 MAJ = 'Major area, region, country or area'
 
-# some global variables that most functions need access to:
-populationOld = []
-populationNew = []
-allIndexes = []
-countryBoundaries = []
-urbanRural = []
-WTP = 0
-WUP = 0
+# Spillover: go into a loop where we remove population above limit and "push" them to the neighboring cells
+# (I was thinking about calling this function "gentrify"...)
+# IMPORTANT: we only select by country and whether the cell is overcrowded,
+# so that we can spill over into other cell types, i.e. from urban to rural
+def spillover(populationProjected, country, limit):
 
-def main():
-    global populationOld, populationNew, allIndexes, countryBoundaries, urbanRural, referencetiff, WTP, WUP, runCountries, postfix
+    overcrowded = np.all((countryBoundaries == country, populationProjected > limit), axis = 0)
+    # for every overcrowded cell, distribute the surplus population randomly among its neighbors
+    for fullCell in allIndexes[overcrowded]:
+        surplus = populationProjected[fullCell] - limit # by how much are we over the limit?
+        # reset those cells to the limit value:
+        populationProjected[fullCell] = limit
+        # and move the extra people to the neighbors:
+        wilsons = getNeighbours(fullCell, shape)
+        rI = np.random.choice(wilsons, surplus)
+        np.add.at(populationProjected, rI, 1)
 
-    logging.info('Starting...')
-
-    logging.info("Reading reference GeoTIFF")
-    referencetiff = os.path.expanduser('~') + '/Dropbox/CISC - Global Population/Asia/GLUR/GLUR_Asia.tif'
-
-    logging.info('Reading CSVs')
-
-    # world URBAN population
-    WUP = transposeDict(csv.DictReader(open(os.path.expanduser('~') + '/Dropbox/CISC - Global Population/Asia/WUP2014Urban_Asia.csv')), "Country Code")
-    # world TOTAL population
-    WTP = transposeDict(csv.DictReader(open(os.path.expanduser('~') + '/Dropbox/CISC - Global Population/Asia/WTP2014_Asia.csv')), "Country Code")
-
-    # if we are running on all countries, make an array that contains all country IDs from WTP top iterate over later:
-    if(runCountries == "all"):
-        logging.info('Running simulation on all countries.')
-        runCountries = []
-        for country in WTP:
-            runCountries.append(country)
-    else:
-        logging.info('Running simulation only on the following countries:')
-        for country in runCountries:
-            logging.info(country)
-
-    logging.info('Reading Numpy arrays')
-
-    # in this dataset: 1=rural, 2=urban
-    urbanRural = np.load(os.path.expanduser('~') + '/Dropbox/CISC - Global Population/Asia/GLUR_asia.tif.npy')
-
-    # save the shape of these arrays for later, so that we
-    # can properly reshape them after flattening:
-    matrix = urbanRural.shape
-
-    # we flatten all arrays to 1D, so we don't have to deal with 2D arrays:
-    urbanRural = urbanRural.ravel()
-    countryBoundaries = np.load(os.path.expanduser('~') + '/Dropbox/CISC - Global Population/Asia/Nations_Asia.tif.npy').ravel()
-
-    # load population raster datasets for 2000 and 2010
-    populationOld = np.load(os.path.expanduser('~') + '/Dropbox/CISC - Global Population/Asia/Population_2000_Asia.tif.npy').ravel()
-    populationNew = np.load(os.path.expanduser('~') + '/Dropbox/CISC - Global Population/Asia/Population_2010_Asia.tif.npy').ravel()
-
-    # these arrays use very small negative numbers as NULL,
-    # let's convert these to NAN
-    populationOld[populationOld==np.nanmin(populationOld)] = np.nan
-    populationNew[populationNew==np.nanmin(populationNew)] = np.nan
-
-    # let's save the 2000 and 2010 tiffs, just to have all the output in one folder:
-    array_to_raster(populationOld.reshape(matrix),
-                             os.path.expanduser('~') + "/Dropbox/CISC - Global Population/Asia/Projections/Population-0-2000_"+postfix+".tif")
-    array_to_raster(populationNew.reshape(matrix),
-                             os.path.expanduser('~') + "/Dropbox/CISC - Global Population/Asia/Projections/Population-0-2010_"+postfix+".tif")
-
-    # make an array of all indexes; we'll use this later:
-    allIndexes = np.arange(countryBoundaries.size)
-
-    logging.info("Growing population...")
-
-    for run in range(RUNS):
-
-        logging.info( "Run no. " + str(run))
-
-        year = 2020
-        step = 10
-        while year <= 2050:
-
-            # start by applying a linear projection to the WHOLE raster?
-            if(projectingLinear):
-                populationProjected = projectLinear(populationOld, populationNew)
-            else:
-                # if not, we'll just start from the last raster:
-                populationProjected = populationNew
-
-            # loop through countries:
-            for country in runCountries:
-
-                logSubArraySizes(populationProjected, year, country)
-
-                # adjust for the difference between raster and csv projection data:
-                adjustPopulation(populationProjected, year, country)
-
-                logging.info(" ----------------- ")
+    print populationProjected.reshape(shape)
+    return populationProjected
 
 
-            # Save to GeoTIFF
-            logging.info('Saving GeoTIFF.')
-            # transform back to 2D array with the original dimensions:
-
-            array_to_raster(populationNew.reshape(matrix),
-                                     os.path.expanduser('~') + "/Dropbox/CISC - Global Population/Asia/Projections/Population-"+str(run)+"-"+str(year)+"_"+postfix+".tif")
-
-            # prepare everything for the next iteration
-
-            populationOld = populationNew
-            populationNew = populationProjected
-            year = year + step
-
-    logging.info('Done.')
-
-
-########################################################
-# Some convenience functions
-########################################################
-
-def logSubArraySizes(populationProjected, year, country):
+def logSubArraySizes(populationProjected, year, country, WTP, countryBoundaries, urbanRural):
 
     logging.info("  ----   ")
     logging.info("Array sizes for " + WTP[str(country)][MAJ])
@@ -155,7 +44,7 @@ def logSubArraySizes(populationProjected, year, country):
 # logs the difference for urban and rural population
 # between whats in the populationProjected and the
 # DESA population projection CSV
-def logDifference(populationProjected, year, country):
+def logDifference(populationProjected, year, country, WTP, WUP, countryBoundaries, urbanRural):
     urbraster = np.sum(populationProjected[
         np.logical_and(countryBoundaries == int(country),
                        urbanRural == urbanCell)])
@@ -177,7 +66,7 @@ def logDifference(populationProjected, year, country):
 
 # this one just compares the numbers from the raster to the CSV
 # and then calls the corresponding functions to add or remove people.
-def adjustPopulation(populationProjected, year, country):
+def adjustPopulation(populationProjected, year, country, WTP, WUP, countryBoundaries, urbanRural, allIndexes):
 
     # figure out the difference between our linear projection
     # and what's in the table:
@@ -198,7 +87,7 @@ def adjustPopulation(populationProjected, year, country):
 
     # This probably slows things down a bit... we've already computed the
     # required values, let's just use these...
-    logDifference(populationProjected, year, country)
+    logDifference(populationProjected, year, country, WTP, WUP, countryBoundaries, urbanRural)
 
     logging.info("Adjusting")
 
@@ -206,30 +95,30 @@ def adjustPopulation(populationProjected, year, country):
     if (urbDiff > 0):  # add people
         logging.info("adding urban population")
         populationProjected = addPopulation(populationProjected, urbDiff,
-                                            country, urbanCell)
+                                            country, urbanCell, WTP, WUP, countryBoundaries, urbanRural, allIndexes)
     else:   # remove people
         logging.info("removing urban population")
         populationProjected = removePopulation(populationProjected,
                                                np.abs(urbDiff), country,
-                                               urbanCell)
+                                               urbanCell, WTP, WUP, countryBoundaries, urbanRural, allIndexes)
 
     # and rural:
     if (rurDiff > 0):  # add people
         logging.info("adding rural population")
         populationProjected = addPopulation(populationProjected, rurDiff,
-                                            country, ruralCell)
+                                            country, ruralCell, WTP, WUP, countryBoundaries, urbanRural, allIndexes)
     else:   # remove people
         logging.info("removing rural population")
         populationProjected = removePopulation(populationProjected,
                                                np.abs(rurDiff), country,
-                                               ruralCell)
+                                               ruralCell, WTP, WUP, countryBoundaries, urbanRural, allIndexes)
 
-    logDifference(populationProjected, year, country)
+    logDifference(populationProjected, year, country, WTP, WUP, countryBoundaries, urbanRural)
 
     return populationProjected
 
 
-def addPopulation(populationProjected, pop, country, cellType):
+def addPopulation(populationProjected, pop, country, cellType, WTP, WUP, countryBoundaries, urbanRural, allIndexes):
 
     try:
         randoms = np.all((countryBoundaries == int(country),
@@ -253,7 +142,7 @@ def addPopulation(populationProjected, pop, country, cellType):
 
 
 
-def removePopulation(populationProjected, pop, country, cellType):
+def removePopulation(populationProjected, pop, country, cellType, WTP, WUP, countryBoundaries, urbanRural, allIndexes):
 
     try:
         # Added the condition that the cell has to have more than 0 population
@@ -355,8 +244,7 @@ def projectLinear(first, second):
 
 
 # saves a raster as a geotiff to dst_filename
-def array_to_raster(array, dst_filename):
-    global referencetiff
+def array_to_raster(array, dst_filename, referencetiff):
 
     reffile = gdal.Open(referencetiff)
 
@@ -377,15 +265,3 @@ def array_to_raster(array, dst_filename):
     dataset.GetRasterBand(1).SetNoDataValue(-3.4028230607371e+38)
     dataset.GetRasterBand(1).WriteArray(array)
     dataset.FlushCache()  # Write to disk.
-
-
-
-if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO,  # toggle this between INFO for debugging and ERROR for "production"
-                        filename='output-'+datetime.utcnow().strftime("%Y%m%d")+'.log',
-                        filemode='w',
-                        format='%(asctime)s, line %(lineno)d %(levelname)-8s %(message)s')
-    try:
-        main()
-    except Exception, e:
-        logging.exception(e)
