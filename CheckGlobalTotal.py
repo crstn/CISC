ecf14653c442fd0c4fd1d4b8241ab37d5d0fa2e0
Carsten Keßler <carsten.kessler@hunter.cuny.edu>
2016-11-11 13:12:49 +0100

    Notifications added, minor fix

--- CheckGlobalTotal.py
+++ CheckGlobalTotal.py
@@ -1,13 +1,15 @@
 import os
 import numpy as np
 import PopFunctions as pop
+import pync
 
 os.chdir(os.path.expanduser('~') + '/Dropbox/CISC Data/IndividualCountries/Projections/Global');
 
 for filename in os.listdir('.'):
     if filename.endswith(".tiff") and filename.startswith("pop-") :
         data = pop.openTIFFasNParray(filename);
-        # small numbers = NAN, set to 0:
-        data[data < 0] = 0
+        # -1 = NAN, set to 0:
+        data[data<0] = 0
+        print filename + ": " + "{:,}".format(np.nansum(data))
 
-        print filename + ": " + "{:,}".format(np.sum(data))
+pync.Notifier.notify('Global totals checked', title='Python')
