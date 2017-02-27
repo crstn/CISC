8630c593c5833746e33f6c94ad6ad97c78f3d599
Maximilian Ioffe <maxioffe@gmail.com>
2017-02-26 23:14:34 -0500

    Changes to output
    Write to .txt instead of .csv
    Decimal instead of scientific notation for population
    Added name lookup for cities
    Using utf-8 instead of ASCII to preserve special characters in names

--- citiesCalc.py
+++ citiesCalc.py
@@ -1,6 +1,7 @@
 from osgeo import gdal
 import numpy as np
-import csv
+import csv               #for reading csv
+import codecs            #for utf-8
 
 #load cities numpy array
 cities = np.load('/Users/mikmaks/Documents/Jobs/CISC/SDEI-Global-UHI/sdei-global-uhi-2013.npy')
@@ -13,13 +14,26 @@ pop[pop < 0] = np.nan #set all population values below 0 as NaN
 
 uniqueCities = np.unique(cities) #get a SORTED list of all city ID's
 
-with open('citiesPop.csv', 'wb') as csvfile:
-    citiesCSV = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL) #open the csv file to write to
-    citiesCSV.writerow(['ID', 'Total Population']) #write the header row
+#open cities CSV file for names
+with open('SDEI-Global-UHI/CitiesAttributes.csv', 'r') as citiesFile:
+    cityNames = csv.DictReader(citiesFile)
 
-    #go through all the cities
-    for city in uniqueCities:
-        #if the city ID is not 0 (0 is not a city)
-        if city != 0:
-            totalPopForCity = np.nansum(pop[cities == city]) #calculate the total population for the city
-            citiesCSV.writerow([city, totalPopForCity]) #write the row to the csv
+    #open output file
+    with codecs.open('citiesPop.txt', 'w', 'utf-8') as outputFile:
+        outputFile.write('ID, Name, Total Population\n') #write the header row
+
+        #go through all the cities
+        for city in uniqueCities:
+            #if the city ID is not 0 (0 is not a city)
+            if city != 0:
+                totalPopForCity = np.nansum(pop[cities == city]) #calculate the total population for the city
+
+                #find the name correspoonding to the city ID
+                name = ""
+                for row in cityNames:
+                    if (row['URBID'] == str(city)):  #if the ID matches, get the name and break
+                        name = unicode(row['NAME'], 'utf8')
+                        break
+
+                newLine = str(city) + ',"' + name + '",' + str('%f' % totalPopForCity) + '\n'
+                outputFile.write(newLine) #write the row to the csv
