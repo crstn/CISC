7f59a537043d9d5dfa328a902bfe85ee0905905e
Carsten Keßler <carsten.kessler@hunter.cuny.edu>
2017-02-28 11:17:26 +0100

    Minor changes

--- citiesCalc.py
+++ citiesCalc.py
@@ -2,20 +2,23 @@ from osgeo import gdal
 import numpy as np
 import csv               #for reading csv
 import codecs            #for utf-8
+import os
 
-#load cities numpy array
-cities = np.load('/Users/mikmaks/Documents/Jobs/CISC/SDEI-Global-UHI/sdei-global-uhi-2013.npy')
-
-#load pop raster and convert to array
-src = gdal.Open('/Users/mikmaks/Documents/Jobs/CISC/Pop_2010_clipped.tiff', gdal.GA_Update)
+print "loading population raster"
+src = gdal.Open(os.path.expanduser('~') + '/Dropbox/CISC Data/Population 2010 Raster/Pop_2010_clipped.tiff', gdal.GA_Update)
 band = src.GetRasterBand(1)
+print "converting to array"
 pop = np.array(band.ReadAsArray())
 pop[pop < 0] = np.nan #set all population values below 0 as NaN
 
+os.chdir(os.path.expanduser('~') + '/Dropbox/CISC Data/SDEI-Global-UHI')
+
+print "loading cities numpy array"
+cities = np.load('sdei-global-uhi-2013.npy')
 uniqueCities = np.unique(cities) #get a SORTED list of all city ID's
 
 #open cities CSV file for names
-with open('SDEI-Global-UHI/CitiesAttributes.csv', 'r') as citiesFile:
+with open('CitiesAttributes.csv', 'r') as citiesFile:
     cityNames = csv.DictReader(citiesFile)
 
     #open output file
@@ -26,7 +29,7 @@ with open('SDEI-Global-UHI/CitiesAttributes.csv', 'r') as citiesFile:
         for city in uniqueCities:
             #if the city ID is not 0 (0 is not a city)
             if city != 0:
-                totalPopForCity = np.nansum(pop[cities == city]) #calculate the total population for the city
+                totalPopForCity = int(np.nansum(pop[cities == city])) #calculate the total population for the city
 
                 #find the name correspoonding to the city ID
                 name = ""
