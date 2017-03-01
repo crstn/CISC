2183483a3320c31d70a1e85d78161be897157dbd
Carsten Keßler <carsten.kessler@hunter.cuny.edu>
2017-03-01 11:58:35 +0100

    Changes make sure that the global output is not overwritten

--- citiesCalc.py
+++ citiesCalc.py
@@ -1,32 +1,74 @@
+#run the script as "citiesCalc.py" for calculation of all cities or
+#"citiesCalc.py 'Moscow' 'New York' etc. for calculation of those cities.
+
 from osgeo import gdal
 import numpy as np
-import csv               #for reading csv
-import codecs            #for utf-8
-import os
+import csv             #for reading csv
+import codecs          #for utf-8
+import os, sys
 
-print "loading population raster"
-src = gdal.Open(os.path.expanduser('~') + '/Dropbox/CISC Data/Population 2010 Raster/Pop_2010_clipped.tiff', gdal.GA_Update)
-band = src.GetRasterBand(1)
-print "converting to array"
-pop = np.array(band.ReadAsArray())
-pop[pop < 0] = np.nan #set all population values below 0 as NaN
+del sys.argv[0]  #delete the first argument (the script name)
 
-os.chdir(os.path.expanduser('~') + '/Dropbox/CISC Data/SDEI-Global-UHI')
+datadir = os.path.expanduser('~') + '/Dropbox/CISC Data/SDEI-Global-UHI'
 
-print "loading cities numpy array"
-cities = np.load('sdei-global-uhi-2013.npy')
-uniqueCities = np.unique(cities) #get a SORTED list of all city ID's
+# Make sure we don't overwrite an existing citiesPop.csv, this one takes
+# hours to generate!
+if (len(sys.argv) == 0) and (os.path.exists(datadir+'/citiesPop.csv')):
+    print "citiesPop.csv already exists. If you want to recalculate it, delete it first."
+    sys.exit()
 
 #open cities CSV file for names
-with open('CitiesAttributes.csv', 'r') as citiesFile:
+with open(os.path.expanduser('~') + '/Dropbox/CISC Data/SDEI-Global-UHI/CitiesAttributes.csv', 'rb') as citiesFile:
     cityNames = csv.DictReader(citiesFile)
 
+    citiesToCalculate = []
+    #find cities passed in as arguments
+    for argument in sys.argv:
+        cityExists = False
+        for row in cityNames:
+            if (row['NAME'] == argument):  #if the name is found, add it to the search list
+                cityExists = True
+                print argument, 'found'
+                citiesToCalculate.append(int(row['URBID']))
+                break
+        if (not cityExists):
+            print argument, 'NOT found'
+
+        citiesFile.seek(0)  #reset the cities csv to be back at the start (so search begins from the start again)
+
+    print
+
+    print "loading population raster"
+    src = gdal.Open(os.path.expanduser('~') + '/Dropbox/CISC Data/Population 2010 Raster/Pop_2010_clipped.tiff', gdal.GA_Update)
+    band = src.GetRasterBand(1)
+    print "converting to array"
+    pop = np.array(band.ReadAsArray())
+    pop[pop < 0] = np.nan  #set all population values below 0 as NaN
+
+    print
+
+    os.chdir(datadir)
+
+    print "loading cities numpy array"
+    cities = np.load('sdei-global-uhi-2013.npy')
+    #either calculate data for all cities or ust ones passed
+    if (len(citiesToCalculate) == 0):
+        citiesToCalculate = np.unique(cities)  #get a SORTED list of all city ID's
+        print 'Outputting data for all cities'
+        filename = 'citiesPop.csv'
+    else:
+        citiesToCalculate = sorted(citiesToCalculate)  #if cities are passed, sort them (to make name lookup efficient)
+        filename = 'citiesPop - '+' - '.join(sys.argv)+'.csv'
+    print
+
+    print "Saving numbers to " + filename
+
     #open output file
-    with codecs.open('citiesPop.txt', 'w', 'utf-8') as outputFile:
-        outputFile.write('ID, Name, Total Population\n') #write the header row
+    with codecs.open(filename, 'w', 'utf-8') as outputFile:
+        outputFile.write('ID, Name, Total Population\n')  #write the header row
 
         #go through all the cities
-        for city in uniqueCities:
+        for city in citiesToCalculate:
             #if the city ID is not 0 (0 is not a city)
             if city != 0:
                 totalPopForCity = int(np.nansum(pop[cities == city])) #calculate the total population for the city
