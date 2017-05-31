650f9ae9e6a6b2ac1d74cfbd3a2cd98912b9abdb
Carsten Keßler <carsten.kessler@hunter.cuny.edu>
2017-05-31 07:43:11 +0200

    Added check for cells within cities

--- Check2010Zeros.py
+++ Check2010Zeros.py
@@ -8,29 +8,41 @@ pync.Notifier.notify('Reading population raster', title='Python')
 p = pop.openTIFFasNParray(os.path.expanduser('~') + '/Dropbox/CISC Data/Population 2010 Raster/Pop_2010_clipped.tiff')
 pync.Notifier.notify('Reading urban/rural raster', title='Python')
 u = pop.openTIFFasNParray(os.path.expanduser('~') + '/Dropbox/CISC Data/GLUR Raster/GRUMP_UrbanRural.tiff')
+pync.Notifier.notify('Reading cities raster', title='Python')
+c = pop.openTIFFasNParray(os.path.expanduser('~') + '/Dropbox/CISC Data/SDEI-Global-UHI/sdei-global-uhi-2013.tiff')
 
 # -1 = NAN, set to 0:
 p[p<0] = 0
 
 # select cells with 1 person or less:
 nopop = p <= 1
+# select cells that are on land
 onland = u >= 1
+# select cells that are in an SDEI city
+outsidecities = c > 0
 
-
+# sanity check:
 print "{:,}".format(p.size)
 print "{:,}".format(u.size)
+print "{:,}".format(c.size)
 
 print "# Cells without population"
 print "{:,}".format(p[nopop].size)
 
 print "# Cells ON LAND without population"
-
 print "{:,}".format(p[np.all((nopop, onland), axis=0)].size)
-
 
 noPop = np.zeros(p.shape, dtype = np.int)
 noPop[np.all((nopop, onland), axis=0)] = 1
 
 pop.array_to_raster(noPop, os.path.expanduser('~') + '/Desktop/noPop-1.tiff', os.path.expanduser('~') + '/Dropbox/CISC Data/Population 2010 Raster/Pop_2010_clipped.tiff')
+
+print "# Cells ON LAND that are WITHIN an SDEI city, but without population"
+print "{:,}".format(p[np.all((nopop, onland, outsidecities), axis=0)].size)
+
+noPopInCities = np.zeros(p.shape, dtype = np.int)
+noPopInCities[np.all((nopop, onland, outsidecities), axis=0)] = 1
+
+pop.array_to_raster(noPopInCities, os.path.expanduser('~') + '/Desktop/noPopInCities.tiff', os.path.expanduser('~') + '/Dropbox/CISC Data/Population 2010 Raster/Pop_2010_clipped.tiff')
 
 pync.Notifier.notify('Done', title='Python')
