cf21e5eb5183bc17dca05d31d061c80a2c267dd2
Carsten <kessler@plan.aau.dk>
2019-04-04 12:20:26 +0200

    Script for downsampling added

---
+++ downsample_all.py
@@ -0,0 +1,40 @@
+import os
+
+# This script will loop through our population files and
+# downsample them to the same resolution as the SEDAC population
+# projection grids; see https://github.com/crstn/CISC/wiki/Downsampling-our-projection-rasters-for-comparison-with-SEDAC-data for a detailed explanation.
+
+
+datadir = os.path.expanduser('~') + '/Dropbox/CISCdata/OneRun/'
+outputdir = os.path.expanduser('~') + '/Dropbox/CISCdata/OneRun/downsampled'
+
+# creates a directoy if it doesn't exist yet
+def makeSafe(dir):
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+
+makeSafe(outputdir)
+
+# loop through all population and urbanization layers and downsample them
+for model in ['GRUMP', 'GlobCover']:
+
+    makeSafe(outputdir+'/'+model)
+
+    for ssp in range(1,6): # !!!
+
+        makeSafe(outputdir+'/'+model+'/SSP' + str(ssp))
+
+        for year in range(2010,2101,10): # !!!
+
+            # downsample:
+            infile = datadir + model + '/SSP' + str(ssp) + '/popmean-' + str(year) +'.tiff'
+            interfile = outputdir+'/'+model+'/SSP' + str(ssp) +'/inter-'+ str(year) +'.tiff'
+            outfile = outputdir+'/'+model+'/SSP' + str(ssp) +'/pop-'+ str(year) +'.tiff'
+
+            os.system("gdalwarp -te -180.0000000 -55.8750000 180.0000000 83.7500000 -ts 2880 1117 -wo NUM_THREADS=ALL_CPUS -co NUM_THREADS=ALL_CPUS -co COMPRESS=LZW -r average -srcnodata -2147483648 "+infile+" "+interfile)
+
+            # multiply
+            os.system('gdal_calc.py -A '+interfile+' --co NUM_THREADS=ALL_CPUS --co COMPRESS=LZW --outfile='+outfile+' --calc="A*225"')
+
+            # delete intermedia file
+            os.system('rm '+interfile)
